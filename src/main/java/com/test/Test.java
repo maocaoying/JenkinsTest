f6ddcb69ab9b43559e7e5934b05677d2dcf5c123
muweixiafang <muweixiafang@163.com>
2017-11-22 15:27:50 +0800

    test

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Kydu1024 on 22/11/2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
+}
